1a563b783e6704ed5701b98a0b5279d36eab0584
Jason Tame <jason@advantagelearn.com>
2022-02-25 16:26:39 +0200

    Add translations for API errors

---
+++ resources/lang/en/api.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    '400' => 'The data sent was invalid',
+    '401' => 'Request unauthorized',
+    '403' => 'The request was forbidden or requires verification',
+    '404' => 'The requested resource could not be found',
+    '405' => 'Request method not allowed',
+    '429' => 'Too many attempts',
+    '500' => 'Internal server error',
+    '501' => 'Unsupported response or grant type',
+    '503' => 'The server is temporarily unavailable'
+];
