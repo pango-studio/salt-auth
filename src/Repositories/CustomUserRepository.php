e2b14f4e883a0b4e06024ad16eb528d435836879
Luke Siedle <ljsiedle@gmail.com>
2022-07-28 13:52:12 +0100

    Adds check for existing user, avoiding repeated calls to Auth0

--- src/Repositories/CustomUserRepository.php
+++ src/Repositories/CustomUserRepository.php
@@ -26,35 +26,34 @@ class CustomUserRepository implements Repository
     public function fromSession(
         array $data
     ): ?\Illuminate\Contracts\Auth\Authenticatable {
-        $user = User::updateOrCreate(
-            [
-                'email' => $data['email'],
-            ],
-            [
-                'name' => $data['name'],
-            ]
-        );
 
-        // Check for auth0 user
-        $user_data = (new Auth0ApiRequester())->searchAuth0UserByEmail($user->email);
-        $password = isset($data['password']) ? $data['password'] : null;
-        if (empty($user_data)) {
-            $auth0_user = (new Auth0ApiRequester())->createAuth0User($user->email, $user->name, $password);
-            $user->sub = $auth0_user->user_id;
-            $user->save();
-        } else {
+        // Always create the user if it doesn't exist yet
+        $user = $this->upsertUser($data);
 
-            /**
-             * This method of account retrieval is risky as there might be multiple accounts
-             * associated with a single email address. When creating a new user though, its
-             * still the most reliable identifier we have available
-             */
-            $user->sub = $user_data[0]->user_id;
-            $user->save();
+        // This will only be true for a new signup
+        if ( $user->wasRecentlyCreated ) {
 
-            // The user has now set another password
-            if ($password) {
-                (new Auth0ApiRequester())->changePassword($user_data[0]->user_id, $password);
+            // Search for an existing user on Auth0
+            $user_data = (new Auth0ApiRequester())->searchAuth0UserByEmail($user->email);
+            $password = isset($data['password']) ? $data['password'] : null;
+            if (empty($user_data)) {
+                $auth0_user = (new Auth0ApiRequester())->createAuth0User($user->email, $user->name, $password);
+                $user->sub = $auth0_user->user_id;
+                $user->save();
+            } else {
+
+                /**
+                 * This method of account retrieval is risky as there might be multiple accounts
+                 * associated with a single email address. When creating a new user though, its
+                 * still the most reliable identifier we have available
+                 */
+                $user->sub = $user_data[0]->user_id;
+                $user->save();
+
+                // The user has now set another password
+                if ($password) {
+                    (new Auth0ApiRequester())->changePassword($user_data[0]->user_id, $password);
+                }
             }
         }
 
@@ -70,5 +69,22 @@ class CustomUserRepository implements Repository
         array $user
     ): ?\Illuminate\Contracts\Auth\Authenticatable {
         return null;
+    }
+
+    /**
+     * Get an existing user or create a new one
+     *
+     * @param array $profile - Auth0 profile
+     *
+     * @return User
+     */
+    protected function upsertUser($profile)
+    {
+        $user = User::firstOrCreate(['email' => $profile['email']], [
+            'sub' => $profile['sub'] ?? '',
+            'name' => $profile['name'] ?? '',
+        ]);
+
+        return $user;
     }
 }
